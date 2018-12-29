908651f4bb909e8b2fe820aaaf4397dd134dab3e
ChemJeff <ChemJeff@pku.edu.cn>
2018-12-30 02:38:38 +0800

    implements dataloader part

---
+++ utils/dataLoader.py
@@ -0,0 +1,155 @@
+# coding=utf-8
+
+'''
+Load preprocessed data given in .pkl file and convert it
+to the format used in pyTorch network 
+code for python 3.x and pyTorch == 0.4
+'''
+
+import os
+import argparse
+import csv
+import nltk
+import numpy as np
+import random
+import torch
+import torch.utils.data as data
+import pickle
+import multiprocessing
+
+
+class DataSet(data.Dataset) :
+    '''
+    自定义DataSet类型，需要实现__init__,__getitem__,__len__方法
+    '''
+
+    def __init__(self, opt) :
+        self.opt = opt
+        # self.split = self.opt.split     # in {'train', 'test', 'val'}
+        with open(self.opt.vocab, 'rb') as f :
+            self.word2id, self.id2word = pickle.load(f)
+        print("Vocab from \'%s\' loaded" % (self.opt.vocab))
+        self.vocab = self.word2id.keys()
+        self.vocab_size = len(self.vocab)
+        print("Vocab size: %d\n" % (self.vocab_size))       
+        self.dataset = self.corpus_covert(self.opt.corpus)
+        self.datalen = len(self.dataset)
+        print("Dataset from \'%s\' loaded" % (self.opt.corpus))
+        print("Dataset split: %s\n" %(self.split))
+
+
+    def corpus_covert(self, corpus_path):
+        corpus = []
+        with open(corpus_path, 'rb') as f:
+            corpus_raw = pickle.load(f)
+            self.split = corpus_raw['split']
+            for idx, sents in enumerate(corpus_raw['data']):
+                sent_ids = []
+                for sent in sents:
+                    ids = [self.word2id[self.opt.BOS]] + \
+                        [self.word2id[word] \
+                        if word in self.vocab \
+                        else self.word2id[self.opt.UNK]
+                        for word in sent] \
+                        + [self.word2id[self.opt.EOS]]
+                    sent_ids.append(ids)
+                if self.split == 'val':     #这个数据集包含label的信息
+                    label = corpus_raw['label'][idx]
+                    corpus.append([sent_ids, label])
+                else:
+                    corpus.append(sent_ids)
+        return corpus
+
+    def __getitem__(self, index) :
+        '''
+        根据数据集划分的不同，处理也有所不同，
+        如果是train，那么需要随机抽取一个错误的结尾，
+        并且随机放在1号句子或者2号句子的位置，
+        然后生成对应的label。
+        如果是test，那么没有对应的label返回
+        如果是val，直接返回6个句子以及对应label
+        '''
+        if self.split == 'val':
+            sent_ids, label = self.dataset[index]
+            return (sent_ids, label)
+        elif self.split == 'test':
+            sent_ids = self.dataset[index]
+            return (sent_ids, )     # 额外包一层tuple方便后续统一处理
+        else:
+            assert self.split == 'train'
+            sent_ids = self.dataset[index][:4]
+            rand_idx = index
+            while(rand_idx == index):
+                rand_idx = random.randint(0, self.datalen - 1)
+            prob_front = random.random()    
+            # 将正确答案平均分布在两个位置上，避免模型直接学习答案分布
+            if prob_front <= 0.5:
+                sent_ids.append(self.dataset[index][4])
+                sent_ids.append(self.dataset[rand_idx][4])
+                label = [1, 0]
+            else:
+                sent_ids.append(self.dataset[rand_idx][4])
+                sent_ids.append(self.dataset[index][4])
+                label = [0, 1]
+            return (sent_ids, label)
+
+    def __len__(self) :
+        return self.datalen
+
+
+def packBatch(batch_in_list) :
+    '''
+    return a torch.nn.utils.rnn.PackedSequence and indices to restore 
+    the original order for a batch sorted by length in a decreasing order
+    '''
+    item_count = len(batch_in_list[0])
+    split = 'train' if item_count == 2 else 'test'
+    batch_len = [x for x in map(lambda item:len(item[0]), batch_in_list)]
+    # print(batch_len)
+    _, idx_sort = torch.sort(torch.tensor(batch_len), dim=0, descending=True)
+    _, idx_unsort = torch.sort(idx_sort, dim=0)
+    word_batch_in_list = [batch_in_list[i][0] for i in idx_sort]
+    # batch_in_list.sort(key=lambda x:len(x), reverse=True)
+    # print(word_batch_in_list)
+    packed_words = torch.nn.utils.rnn.pack_sequence(word_batch_in_list)
+    # print(packed_words)
+    if split == 'train' :
+        tag_batch_in_list = [batch_in_list[i][1] for i in idx_sort]
+        packed_tags = torch.nn.utils.rnn.pack_sequence(tag_batch_in_list)
+        return (packed_words, packed_tags), idx_unsort
+    return packed_words, idx_unsort
+
+def collate(batch_in_list) :
+    '''
+    collate function to convert raw data fetched from dataset
+    to a torch.nn.utils.rnn.PackedSequence for LSTM input
+    considering usage, return a lot of auxiliary info as well
+    '''
+    batch_size = len(batch_in_list)
+    words = [item[1] for item in batch_in_list]
+    for i in range(batch_size) :
+        batch_in_list[i] = [torch.tensor(item, dtype=torch.int64) for item in batch_in_list[i][0]]
+    packed, idx_unsort = packBatch(batch_in_list)
+    return packed, idx_unsort, words
+
+def testcollate(data_in):
+    return data_in
+
+if __name__ == "__main__" :
+    opt = argparse.Namespace()
+    opt.BOS = '<p>'
+    opt.EOS = '</p>'
+    opt.UNK = '<unk>'
+    opt.corpus = './train.pkl'
+    opt.vocab = './vocab.pkl'
+    opt.embedding = './vocab_embed.pkl' 
+    opt.encoding = 'utf-8'
+    dataset = DataSet(opt)
+    dataloader = data.DataLoader(dataset, batch_size=1, collate_fn=testcollate)
+    sample = 10
+    for item in dataloader:
+        sample -= 1
+        if sample < 0 :
+            continue
+        print(item, sep='\n')
+    pass
---
+++ utils/dataPreprocess.py
@@ -0,0 +1,126 @@
+# coding=utf-8
+
+'''
+Load data given in .csv file , do some preprocessing 
+and then save to .pkl files
+code for python 3.x
+'''
+
+import csv
+import pickle
+import nltk
+import numpy as np
+
+BOS = '<p>'
+EOS = '</p>'
+UNK = '<unk>'
+BOS_ID = 0
+EOS_ID = 1
+UNK_ID = 2
+EMBED_DIM = 300
+
+def add_freq(word, freq_dict):
+    if word not in freq_dict.keys():
+        freq_dict[word] = 1
+    else:
+        freq_dict[word] += 1
+
+def build_vocab_list(file_path_list=['train.csv', 'val.csv', 'test.csv'], encoding='utf-8', out_file=None):
+    vocab_freq = dict()
+    for file_path in file_path_list:
+        with open(file_path, 'r', encoding=encoding) as f:
+            csvreader = csv.reader(f)
+            next(csvreader)     # 跳过csv文件的标题行
+            for entry in csvreader:
+                if first_line:
+                    first_line = False
+                    continue
+                entry = entry[:-1] if file_path == 'val.csv' else entry
+                for sent in entry:
+                    words = nltk.tokenize.word_tokenize(sent)
+                    add_freq(BOS, vocab_freq)
+                    add_freq(EOS, vocab_freq)
+                    for word in words:
+                        add_freq(word, vocab_freq)
+
+    return vocab_freq
+
+def build_word_vec(vocab_freq, w2v_path='./embedding/crawl-300d-2M.vec'):
+    word_vec = dict()
+    w2i = dict()
+    i2w = dict()
+    i2v = []
+    idx = UNK_ID + 1    # 当前分配的id号
+    with open(w2v_path, 'r', encoding='utf-8') as f:
+        line = f.readline().strip()
+        for line in f:
+            word, vec = line.split(' ', 1)
+            if word in vocab_freq.keys():
+                word_vec[word] = np.fromstring(vec, sep=' ')
+        word_vec[UNK] = np.zeros(EMBED_DIM)  # 用全0向量作为OOV词汇的词向量
+    print('Found %s(/%s) words with w2v vectors' % (len(word_vec), len(vocab_freq)))
+    sorted_freq = sorted(vocab_freq.items(), key=lambda x:x[1], reverse=True) # 按频率排序
+    w2i[BOS] = BOS_ID
+    w2i[EOS] = EOS_ID
+    w2i[UNK] = UNK_ID
+    for (word, freq) in sorted_freq:  # 按出现频率分配词的id号，建立w2i索引
+        if word in {BOS, EOS, UNK}:  # 已经分配了index的特殊标记
+            continue
+        if word in word_vec.keys():
+            w2i[word] = idx
+            idx += 1
+    for i in w2i.items():   # 反向建立一个i2w索引
+        i2w[i[1]] = i[0]
+    for i in range(idx):    # 建立id号到词向量的矩阵
+        i2v.append(word_vec[i2w[i]])
+    i2v = np.array(i2v)
+    print('Vocab size: %d, including {<p>, </p>, <unk>}' % (idx))
+    return w2i, i2w, i2v
+
+def corpus_tokenize(corpus_path, split, encoding='utf-8'):
+    '''
+    提前将分词完成的训练数据保存，避免每次读入数据都要现场分词
+    '''
+    assert split in {'train', 'test', 'val'}
+    corpus = dict()
+    corpus['data'] = []
+    corpus['split'] = split
+    if split == 'val':
+        corpus['label'] = []
+    with open(corpus_path, 'r', encoding=encoding) as f:
+        csvreader = csv.reader(f)
+        next(csvreader)         # 跳过csv文件的标题行
+        for entry_raw in csvreader:
+            entry = entry_raw[:-1] if split == 'val' else entry_raw
+            sents = []
+            for sent in entry:
+                words = nltk.tokenize.word_tokenize(sent)
+                sents.append(words)
+            if split == 'val':     # 需要从csv文件中读入label
+                label = [1, 0] if entry_raw[-1] == '1' else [0, 1]
+                corpus['data'].append(sents)
+                corpus['label'].append(label)
+            else:
+                corpus['data'].append(sents)
+    return corpus
+
+if __name__ == "__main__":
+    # vocab = build_vocab_list()
+    # with open('vocab_freq.pkl', 'wb') as f:
+    #     pickle.dump(vocab, file=f)
+    # with open('vocab_freq.pkl', 'rb') as f:
+    #     vocab = pickle.load(f)
+    # w2i, i2w, i2v = build_word_vec(vocab)
+    # with open('vocab.pkl', 'wb') as f:
+    #     pickle.dump((w2i, i2w), file=f)
+    # with open('vocab_embed.pkl', 'wb') as f:
+    #     pickle.dump(i2v, file=f)
+    # with open('vocab.pkl', 'rb') as f:
+    #     w2i, i2w = pickle.load(f)
+    # with open('vocab_embed.pkl', 'rb') as f:
+    #     i2v = pickle.load(f)
+    # print(w2i, i2w, i2v)
+    # corpus = corpus_tokenize('./train.csv', 'train')
+    # with open('train.pkl', 'wb') as f:
+    #     pickle.dump(corpus, file=f)
+    # print(corpus)
